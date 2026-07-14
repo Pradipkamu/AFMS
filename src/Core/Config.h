f61226c73d41030dc1d31c29014ce0005e517068
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-14 18:39:35 +0530

    feat(core): add configuration interface

---
+++ src/Core/Config.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <Arduino.h>
+
+namespace Config {
+bool load();
+const char *machineId();
+const char *machineName();
+const char *wifiSsid();
+const char *wifiPassword();
+}
