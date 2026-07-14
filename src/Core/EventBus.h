c8674ed4a3d406b2ca721a6d07c2cb64b9a6d25f
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-14 18:41:43 +0530

    feat(core): add event bus interface

---
+++ src/Core/EventBus.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <Arduino.h>
+
+enum class EventType : uint8_t {
+  SystemBoot,
+  WifiConnected,
+  WifiDisconnected,
+  MachineReady,
+  ProductionPulse,
+  RejectPulse,
+  IdleStarted,
+  IdleEnded,
+  AlarmActivated,
+  AlarmCleared
+};
+
+struct Event {
+  EventType type;
+  uint32_t timestampMs;
+  int32_t value;
+};
+
+namespace EventBus {
+void begin();
+bool publish(EventType type, int32_t value = 0);
+bool next(Event &event);
+void update();
+uint16_t droppedCount();
+}
