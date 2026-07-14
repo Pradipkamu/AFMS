1990675d3e3c659b5bcae38fc40d71dab5f80f06
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-14 18:49:20 +0530

    feat(machine): expose integrated machine snapshot

--- src/Machine/MachineEngine.h
+++ src/Machine/MachineEngine.h
@@ -1,7 +1,10 @@
 #pragma once
+#include "MachineState.h"
 
 namespace MachineEngine {
 void begin();
 void update();
 bool ready();
+MachineSnapshot snapshot();
+void acknowledgeLossCode(uint16_t lossCode);
 }
