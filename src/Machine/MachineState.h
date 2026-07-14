3d8b1ada0fce6a2ecd970a8734f30254bf9ffc13
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-14 19:32:11 +0530

    Commit 0005: expose live OEE metrics in machine snapshot

--- src/Machine/MachineState.h
+++ src/Machine/MachineState.h
@@ -15,5 +15,12 @@ struct MachineSnapshot {
   uint32_t goodParts;
   uint32_t lastProductionMs;
   uint32_t idleSeconds;
+  uint32_t runSeconds;
+  uint32_t downtimeSeconds;
+  uint32_t targetQuantity;
+  uint16_t availabilityPermille;
+  uint16_t performancePermille;
+  uint16_t qualityPermille;
+  uint16_t oeePermille;
   bool alarmActive;
 };
