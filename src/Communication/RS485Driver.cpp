c3f211f3ef1ab9effba3a44082016488b6214327
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-15 17:44:47 +0530

    Commit 0020: use UART0 for MAX485 Modbus

--- src/Communication/RS485Driver.cpp
+++ src/Communication/RS485Driver.cpp
@@ -1,19 +1,21 @@
 #include "RS485Driver.h"
 
 namespace {
-SoftwareSerial gPort;
 uint8_t gDirectionPin = 255;
 }
 
-void RS485Driver::begin(uint8_t rxPin, uint8_t txPin, uint8_t directionPin, uint32_t baud) {
+void RS485Driver::begin(uint8_t directionPin, uint32_t baud) {
   gDirectionPin = directionPin;
   pinMode(gDirectionPin, OUTPUT);
   digitalWrite(gDirectionPin, LOW);
-  gPort.begin(baud, SWSERIAL_8N1, rxPin, txPin, false, 128);
-  gPort.enableIntTx(false);
+
+  Serial.end();
+  Serial.begin(baud, SERIAL_8N1);
+  Serial.setRxBufferSize(256);
+  flushInput();
 }
 
-SoftwareSerial &RS485Driver::port() { return gPort; }
+HardwareSerial &RS485Driver::port() { return Serial; }
 
 void RS485Driver::setTransmit(bool enabled) {
   digitalWrite(gDirectionPin, enabled ? HIGH : LOW);
@@ -21,5 +23,5 @@ void RS485Driver::setTransmit(bool enabled) {
 }
 
 void RS485Driver::flushInput() {
-  while (gPort.available()) gPort.read();
+  while (Serial.available()) Serial.read();
 }
