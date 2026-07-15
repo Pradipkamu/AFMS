1117b94d5a5d5b4f84ada6ede64654120fb1c798
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-15 17:44:27 +0530

    Commit 0020: migrate RS485 driver to hardware UART

--- src/Communication/RS485Driver.h
+++ src/Communication/RS485Driver.h
@@ -1,10 +1,9 @@
 #pragma once
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 
 namespace RS485Driver {
-void begin(uint8_t rxPin, uint8_t txPin, uint8_t directionPin, uint32_t baud);
-SoftwareSerial &port();
+void begin(uint8_t directionPin, uint32_t baud);
+HardwareSerial &port();
 void setTransmit(bool enabled);
 void flushInput();
 }
