dd8b2b5f7d1fbf6259818ed63037bf9cfcd66da5
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-16 22:38:59 +0530

    Add reject input interlock

--- src/Machine/RejectManager.h
+++ src/Machine/RejectManager.h
@@ -3,6 +3,8 @@
 
 namespace RejectManager {
 void begin(uint8_t pin, uint32_t debounceUs = 50000UL);
+void setEnabled(bool enabled);
+bool enabled();
 bool consumePulse();
 uint32_t total();
 void restore(uint32_t total);
