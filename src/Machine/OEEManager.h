a243c6ed0e6abd491950b849f3096ea7dbec4e76
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-16 15:50:26 +0530

    AFMS audit: add shift counter baselines and OEE persistence

--- src/Machine/OEEManager.h
+++ src/Machine/OEEManager.h
@@ -14,14 +14,26 @@ struct OEESnapshot {
   uint16_t oeePermille;
 };
 
+struct OEEPersistentState {
+  uint32_t measuredElapsedMs;
+  uint32_t plannedShutdownMs;
+  uint32_t downtimeMs;
+  uint32_t productionBaseline;
+  uint32_t rejectBaseline;
+  uint32_t lossSeconds[17];
+};
+
 namespace OEEManager {
 void begin(uint32_t idealCycleTimeMs);
 void update(bool downtime, uint32_t totalParts, uint32_t rejectParts);
 void setIdealCycleTimeMs(uint32_t idealCycleTimeMs);
 void setTargetQuantity(uint32_t targetQuantity);
 void setScheduledShiftElapsedSeconds(uint32_t elapsedSeconds);
+void setCounterBaselines(uint32_t totalParts, uint32_t rejectParts);
 void recordLoss(uint16_t lossCode, uint32_t durationSeconds);
 OEESnapshot snapshot();
 uint32_t lossSeconds(uint16_t lossCode);
+OEEPersistentState persistentState();
+void restorePersistentState(const OEEPersistentState &state);
 void resetShift();
 }
