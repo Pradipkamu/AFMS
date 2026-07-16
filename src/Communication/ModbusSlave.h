6cd199da7b83eaffbb84c3dd2ba96ca2b35e837d
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-16 21:33:48 +0530

    Add Modbus coil support for HMI loss buttons

--- src/Communication/ModbusSlave.h
+++ src/Communication/ModbusSlave.h
@@ -4,6 +4,8 @@
 namespace ModbusSlave {
 void begin(uint8_t slaveId, uint16_t *registers, uint16_t registerCount);
 void update();
+bool consumeCoil(uint16_t address);
+bool coil(uint16_t address);
 bool connected();
 uint32_t requestCount();
 uint32_t errorCount();
