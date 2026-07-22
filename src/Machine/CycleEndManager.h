20dc9ba439aa2104aff3fe37b411064ae57ff968
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-22 22:08:08 +0530

    refactor: add cycle end queue control

--- src/Machine/CycleEndManager.h
+++ src/Machine/CycleEndManager.h
@@ -4,7 +4,9 @@
 namespace CycleEndManager {
 void begin(uint8_t pin, bool activeHigh, uint16_t debounceMs);
 void update();
-bool consumePulse(uint32_t &timestampMs);
+bool consumePulse(uint32_t &timestampUs);
+void setEnabled(bool enabled);
+void clearPending();
 bool active();
 uint32_t droppedPulseCount();
 }
