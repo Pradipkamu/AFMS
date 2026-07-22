3b9cb7ece1e93e371c6527326034c8e1a550ec34
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-22 09:51:16 +0530

    Cycle End 0008: start idle timing from cycle completion

--- src/Machine/IdleManager.h
+++ src/Machine/IdleManager.h
@@ -5,7 +5,7 @@ namespace IdleManager {
 void begin(uint32_t idleDelayMs);
 void setIdleDelayMs(uint32_t idleDelayMs);
 void onProduction();
-void update(bool cycleExpired, uint32_t nowMs, uint32_t lastProductionMs, uint32_t cycleTimeMs);
+void update(bool cycleCompleted, uint32_t nowMs, uint32_t completionTimeMs);
 bool idle();
 bool alarmDue();
 uint32_t idleSeconds();
