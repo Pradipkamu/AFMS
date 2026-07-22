068239582e75a962ee340d525865c0f69a1dcc63
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-22 09:50:36 +0530

    Cycle End 0006: extend cycle manager for input completion mode

--- src/Machine/CycleManager.h
+++ src/Machine/CycleManager.h
@@ -2,10 +2,16 @@
 #include <Arduino.h>
 
 namespace CycleManager {
-void begin(uint32_t cycleTimeMs);
+void begin(uint32_t cycleTimeMs, bool cycleEndEnabled, uint32_t cycleEndTimeoutMs);
 void setCycleTimeMs(uint32_t cycleTimeMs);
 void onProduction(uint32_t nowMs);
+bool onCycleEnd(uint32_t nowMs);
+void update(uint32_t nowMs);
 uint32_t cycleTimeMs();
 uint32_t lastProductionMs();
-bool cycleExpired(uint32_t nowMs);
+uint32_t completionTimeMs();
+bool cycleCompleted();
+bool cycleInProgress();
+bool cycleEndEnabled();
+uint32_t timeoutCount();
 }
