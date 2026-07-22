e125468d7225a5c7b20161e58470a7592c16b7eb
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-22 22:07:25 +0530

    refactor: expose production interrupt microsecond timestamps

--- src/Machine/ProductionManager.h
+++ src/Machine/ProductionManager.h
@@ -3,7 +3,7 @@
 
 namespace ProductionManager {
 void begin(uint8_t pin, uint32_t debounceUs = 50000UL);
-bool consumePulse(uint32_t &timestampMs);
+bool consumePulse(uint32_t &timestampUs);
 void acceptPulse();
 uint32_t total();
 void restore(uint32_t total);
