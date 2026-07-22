8d15e24c3dd08b90d81b403dd53bfd5b8edb68c3
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-22 09:48:12 +0530

    Cycle End 0001: add optional cycle-end input interface

---
+++ src/Machine/CycleEndManager.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <Arduino.h>
+
+namespace CycleEndManager {
+void begin(uint8_t pin, bool activeHigh, uint16_t debounceMs);
+void update();
+bool consumePulse();
+bool active();
+}
