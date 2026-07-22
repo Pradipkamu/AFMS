0f585d9bdfc48b0ec103b7aca131aa1a2be181af
Pradipkamu <126343425+Pradipkamu@users.noreply.github.com>
2026-07-22 12:11:57 +0530

    fix(cycle): validate production pulses before counting

--- src/Machine/ProductionManager.h
+++ src/Machine/ProductionManager.h
@@ -3,10 +3,12 @@
 
 namespace ProductionManager {
 void begin(uint8_t pin, uint32_t debounceUs = 50000UL);
-bool consumePulse();
+bool consumePulse(uint32_t &timestampMs);
+void acceptPulse();
 uint32_t total();
 void restore(uint32_t total);
 void reset();
 void setEnabled(bool enabled);
 bool enabled();
+uint32_t droppedPulseCount();
 }
